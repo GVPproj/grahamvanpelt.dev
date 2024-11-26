7a135e4fde5c919b2a9db8ef774662ae2bfc1563
Graham Van Pelt <grahamvanpelt@gmail.com>
2024-11-26 15:22:38 -0800

    feat: tailwind colours active

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,6 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
+import typography from "@tailwindcss/typography";
 
 export default {
   content: [
@@ -8,11 +10,42 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        serif: ["Sfizia", ...defaultTheme.fontFamily.sans],
+        sans: ["Wotfard", ...defaultTheme.fontFamily.sans],
+      },
+      textColor: {
+        skin: {
+          base: "var(--colour-text-base)",
+          muted: "var(--colour-text-muted)",
+          accent: "var(--colour-accent)",
+          highlight: "var(--colour-highlight)",
+        },
+      },
+      backgroundColor: {
+        skin: {
+          fill: "var(--colour-fill)",
+          "fill-muted": "var(--colour-fill-muted)",
+          accent: "var(--colour-accent)",
+          highlight: "var(--colour-highlight)",
+        },
+      },
+      stroke: {
+        skin: {
+          base: "var(--colour-text-base)",
+          accent: "var(--colour-accent)",
+          highlight: "var(--colour-highlight)",
+        },
+      },
+      borderColor: {
+        skin: {
+          base: "var(--colour-text-base)",
+          accent: "var(--colour-accent)",
+          highlight: "var(--colour-highlight)",
+          muted: "var(--colour-text-muted)",
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [typography],
 } satisfies Config;
